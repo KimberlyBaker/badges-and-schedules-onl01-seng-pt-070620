ab5d2c43dccc2244faf1f2fecb36fe25fd0008b2
Kimberly Baker <bakerk2010@gmail.com>
2020-07-20 05:53:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,14 +10,12 @@ def batch_badge_creator(attendees)
   attendees_array
 end
 
-def assign_rooms(room)
-  room_array = []
-  counter = 1
-  room.each do |attendees|
-    counter += 1
-    room_array << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+def assign_rooms(attendees)
+  room = 0
+  attendees.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
-  room_array
 end
 
 
